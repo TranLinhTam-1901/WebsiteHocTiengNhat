cb24d3842b3fbf9e95bded7c92b9e670a70bc674
Trần Linh Tâm <tranlinhtamtlt@gmail.com>
2026-03-31 23:54:29 +0700

    Update SQL queries for Vocabulary selection
    fix dữ liệu fill

--- Fill_SQL_N5.sql
+++ Fill_SQL_N5.sql
@@ -4443,24 +4443,12 @@ TRUNCATE TABLE "Kanjis" RESTART IDENTITY CASCADE;
 TRUNCATE TABLE "Radicals" RESTART IDENTITY CASCADE;
 TRUNCATE TABLE "RadicalVariants" RESTART IDENTITY CASCADE;
 
-<<<<<<< Updated upstream
 -------------------------------------------------------
 -- SELECT VD
 -------------------------------------------------------
 SELECT * FROM "GrammarGroups"
 WHERE "VocabID" = '014415d9-f006-4558-9a09-f4dcdee4a742';
 
-=======
-TRUNCATE TABLE "Kanjis" RESTART IDENTITY CASCADE;
-TRUNCATE TABLE "Radicals" RESTART IDENTITY CASCADE;
-TRUNCATE TABLE "RadicalVariants" RESTART IDENTITY CASCADE;
 
--------------------------------------------------------
--- SELECT VD
--------------------------------------------------------
-SELECT * FROM "Kanjis"
-WHERE "VocabID" = '014415d9-f006-4558-9a09-f4dcdee4a742';
-
->>>>>>> Stashed changes
 SELECT * FROM "Vocabularies"
 WHERE "Word" = '置く';
