3d9c8a614b5ef3526a7c130ee4782f78d4a15940
TranLinhTam-1901 <tranlinhtamtlt@gmail.com>
2026-04-01 23:07:21 +0700

    fix nhanh dev

--- BE/Migrations/20260401155655_Initial.Designer.cs
+++ BE/Migrations/20260401155655_Initial.Designer.cs
@@ -12,8 +12,8 @@ using QuizzTiengNhat.Models;
 namespace QuizzTiengNhat.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260331103436_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20260401155655_Initial")]
+    partial class Initial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
--- FE/src/env.d.ts
+++ FE/src/env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_API_URL: string;
- 
-}
+  interface ImportMetaEnv {
+    readonly VITE_API_URL: string;
+  }
+
 
 export {};
